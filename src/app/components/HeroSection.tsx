315be1aab788d7401f78d8a768c12d86da9575aa
Dev-knowledge <utzsv1989@gmail.com>
2024-06-25 23:32:54 +0100

    save me

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -6,9 +6,9 @@ function HeroSection() {
   return (
     <section
       id="home"
-      className="h-[550px] lg:h-[600px] xl:h-[610px] lg:max-w-4xl xl:max-w-6xl md:max-w-2xl mx-auto overflow-hidden text-white "
+      className="h-[650px] lg:h-[600px] xl:h-[610px] lg:max-w-4xl xl:max-w-6xl md:max-w-2xl mx-auto overflow-hidden text-white "
     >
-      <div className="relative flex items-center justify-start ">
+      <div className="relative flex lg:flex-row flex-col items-center justify-center ">
         <div className="bg-gradient-to-t from-[#0f0f0f] py-36 -bottom-14 lg:-bottom-[110px] absolute w-full"></div>
         <p
           data-aos="zoom-in"
@@ -18,42 +18,44 @@ function HeroSection() {
         </p>
         <p
           data-aos="fade-right"
-          className={`${lilita.className} absolute top-96 left-6 md:left-24 lg:left-20 text-[28px] text-center lg:text-[32px] xl:text-[44px] max-w-[350px] md:max-w-[480px] lg:max-w-[350px] z-50`}
+          className={`${lilita.className} md:absolute top-96 left-6 md:left-24 lg:left-20 text-[28px] text-center lg:text-[32px] xl:text-[44px] max-w-[350px] md:max-w-[480px] lg:max-w-[350px] z-50`}
         >
           THE SMARTEST AI-POWERED COIN ON BSC.
         </p>
+        <p
+          data-aos="fade-down"
+          className={`lg:absolute lg:top-[550px] top-[520px] right-12 md:right-56 lg:right-36 text-center text-[11px] lg:text-[12px] xl:text-[16px] max-w-[480px] z-50`}
+        >
+          Loufi Coin Unlocks The Incredible Loufi Capability With Ai. <br />{" "}
+          Loufi Coin Is 1000x Smarter Than The Original Coin Empowred.
+        </p>
+        <div className="absolute w-full h-full flex items-center justify-center">
+          <Image
+            data-aos="fade-right"
+            className=" hidden xl:block"
+            src="/images/hero.png"
+            alt="logo"
+            width={500}
+            height={400}
+          />
+          <Image
+            data-aos="fade-right"
+            className=" hidden lg:block xl:hidden"
+            src="/images/hero.png"
+            alt="logo"
+            width={400}
+            height={300}
+          />
+          <Image
+            data-aos="fade-right"
+            className="md:left-[240px]  lg:hidden block"
+            src="/images/hero.png"
+            alt="logo"
+            width={300}
+            height={200}
+          />
+        </div>
       </div>
-      <Image
-        data-aos="fade-right"
-        className="absolute z-0 -top-20 left-[400px] hidden xl:block"
-        src="/images/hero.png"
-        alt="logo"
-        width={500}
-        height={400}
-      />
-      <Image
-        data-aos="fade-right"
-        className="absolute z-0 top-10 left-[280px] hidden lg:block xl:hidden"
-        src="/images/hero.png"
-        alt="logo"
-        width={400}
-        height={300}
-      />
-      <Image
-        data-aos="fade-right"
-        className="absolute z-0 top-10 left-[45px] md:left-[240px]  lg:hidden block"
-        src="/images/hero.png"
-        alt="logo"
-        width={300}
-        height={200}
-      />
-      <p
-        data-aos="fade-down"
-        className={`md:absolute lg:top-[550px] top-[520px] right-12 md:right-56 lg:right-36 text-center text-[11px] lg:text-[12px] xl:text-[16px] max-w-[480px] z-50`}
-      >
-        Loufi Coin Unlocks The Incredible Loufi Capability With Ai. <br /> Loufi
-        Coin Is 1000x Smarter Than The Original Coin Empowred.
-      </p>
     </section>
   );
 }
